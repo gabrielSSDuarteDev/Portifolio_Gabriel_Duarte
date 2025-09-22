06dc73614e54e5165b0dfa7efd3e547da6ed0739
Gabriel Duarte <gabrielSSDuarte.dev@gmail.com>
2025-09-22 16:51:23 -0300

    Chore: style add and themes functions
    Changes on ThemeToggle.tsx and Index.css

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,10 +1,50 @@
-import {Sun} from "lucide-react";
-import {Moon} from "lucide-react";
-import { useState } from "react";
-
+import { Sun } from "lucide-react";
+import { Moon } from "lucide-react";
+import { useState, useEffect, use } from "react";
 
 export const ThemeToggle = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
-    return <button> {isDarkMode ? <Sun/> : <Moon/>}</button>
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-}
+
+ // Verifica o tema salvo no localStorage
+    useEffect(() => { 
+    const savedTheme = localStorage.getItem("theme");   
+    if (savedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+      setIsDarkMode(false);
+    }
+    }, []);
+
+  // Use useEffect para sincronizar o estado com a classe no DOM
+  useEffect(() => {
+    // Verifica o estado inicial do tema
+    if (document.documentElement.classList.contains("dark")) {
+        setIsDarkMode(true);
+    }
+
+  }, []);
+
+  const toggleTheme = () => {
+    // Altera o estado
+    setIsDarkMode(!isDarkMode);
+   localStorage.setItem("theme", isDarkMode ? "light" : "dark");
+
+    // Usa toggle() para adicionar ou remover a classe "dark" de forma simples
+    document.documentElement.classList.toggle("dark");
+    
+ };
+
+    //  Retorna o ícone de sol ou lua com base no estado atual
+  return (
+    <button onClick={toggleTheme}>
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-gray-800" />
+      )}
+    </button>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
+import { ThemeToggle } from "../components/ThemeToggle.tsx";
 
 export const Home = () => {
   return (
 
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
     {/* Theme Toggle*/}
-
+    <ThemeToggle/>
     {/* BackGround Effects */}
 
     {/*NavBar */ }
